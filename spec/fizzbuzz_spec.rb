c7e336b1c9f797a66dc2d24f81a7c51fbe9c6bea
bella-cockrell <arabella.cockrell@gmail.com>
2020-10-12 22:49:08 +0100

    refactoring test code

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,17 +1,9 @@
 require_relative "../lib/fizzbuzz"
 
-context "when I fizzbuzz the number 1" do
+context "when I fizzbuzz the numbers 1, 2, 4" do
     it "I get back a string representing it" do
         expect(fizzbuzz(1)).to eq("1")
-    end
-end
-context "when I fizzbuzz the number 2" do
-    it "I get back a string representing it" do
         expect(fizzbuzz(2)).to eq("2")
-    end
-end
-context "when I fizzbuzz the number 4" do
-    it "I get back a string representing it" do
         expect(fizzbuzz(4)).to eq("4")
     end
 end
