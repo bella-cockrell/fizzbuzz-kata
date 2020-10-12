97fea0f6d97d11433ecf6691f494517ff6d3672b
bella-cockrell <arabella.cockrell@gmail.com>
2020-10-12 22:48:06 +0100

    Refactoring production code

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,9 +1,3 @@
 def fizzbuzz(number)
-        if number == 1
-            return "1"
-        elsif number == 2
-            return "2"
-        else
-            return "4"
-        end
+    return number.to_s
 end
