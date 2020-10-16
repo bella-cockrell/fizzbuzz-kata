f8153dcd380bb7626baee179784e0b00d9fd05a3
bella-cockrell <arabella.cockrell@gmail.com>
2020-10-16 14:45:39 +0100

    Adding fizz and buzz tests, refactoring fizz production code

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,6 +1,8 @@
 def fizzbuzz(number)
-    if number == 3
+    if number % 3 == 0
         return "fizz"
+    elsif number == 5 || number == 10
+        return "buzz"
     else
         return number.to_s
     end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -7,8 +7,18 @@ context "when I fizzbuzz the numbers 1, 2, 4" do
         expect(fizzbuzz(4)).to eq("4")
     end
 end
-context "when I fizzbuzz the number 3" do
+#triangulation is forcing a new behaviour
+context "when I fizzbuzz the number 3, 6, 9 or 12" do
     it "I get back 'fizz'" do
         expect(fizzbuzz(3)).to eq("fizz")
+        expect(fizzbuzz(6)).to eq("fizz")
+        expect(fizzbuzz(9)).to eq("fizz")
+        expect(fizzbuzz(12)).to eq("fizz")
+    end
+end
+context "when I fizzbuzz the number 5" do
+    it "I get back 'buzz'" do
+        expect(fizzbuzz(5)).to eq("buzz")
+        expect(fizzbuzz(10)).to eq("buzz")
     end
 end
