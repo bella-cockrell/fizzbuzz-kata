b8823eee596e0790325b42f499d244cc09f4c382
bella-cockrell <arabella.cockrell@gmail.com>
2020-10-16 14:54:04 +0100

    Refactoring tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,23 +2,39 @@ require_relative "../lib/fizzbuzz"
 
 context "when I fizzbuzz the numbers 1, 2, 4" do
     it "I get back a string representing it" do
-        expect(fizzbuzz(1)).to eq("1")
-        expect(fizzbuzz(2)).to eq("2")
-        expect(fizzbuzz(4)).to eq("4")
+        nonspecial_numbers = {
+            1 => "1",
+            2 => "2",
+            4 => "4"
+        }
+        nonspecial_numbers.each do |input, output|
+            expect(fizzbuzz(input)).to eq(output)
+        end    
     end
 end
 #triangulation is forcing a new behaviour
 context "when I fizzbuzz the number 3, 6, 9 or 12" do
     it "I get back 'fizz'" do
-        expect(fizzbuzz(3)).to eq("fizz")
-        expect(fizzbuzz(6)).to eq("fizz")
-        expect(fizzbuzz(9)).to eq("fizz")
-        expect(fizzbuzz(12)).to eq("fizz")
+        fizz_numbers = {
+            3 => "fizz",
+            6 => "fizz",
+            9 => "fizz", 
+            12 => "fizz"
+        }
+        fizz_numbers.each do |input, output|
+            expect(fizzbuzz(input)).to eq(output)
+        end
     end
 end
 context "when I fizzbuzz the number 5" do
     it "I get back 'buzz'" do
-        expect(fizzbuzz(5)).to eq("buzz")
-        expect(fizzbuzz(10)).to eq("buzz")
+        buzz_numbers = {
+            5 => "buzz",
+            10 => "buzz"
+        }
+        buzz_numbers.each do |input, output|
+        expect(fizzbuzz(input)).to eq(output)
+        end
+
     end
 end
